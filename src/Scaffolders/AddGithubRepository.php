95fd6bf139972b914e6a703eb005520eab1f9b8b
librevlad <librevlad@gmail.com>
2019-04-10 23:03:41 +0300

    wip

--- src/Scaffolders/AddGithubRepository.php
+++ src/Scaffolders/AddGithubRepository.php
@@ -3,10 +3,14 @@
 namespace DevWorkout\Scaffold\Scaffolders;
 
 use Afterflow\Framework\Scaffolder;
+use Github\Client;
+use Github\Exception\RuntimeException;
 
 class AddGithubRepository extends Scaffolder
 {
     protected $question = 'Add github repository?';
+
+    protected $github;
 
     public function handle()
     {
@@ -14,42 +18,68 @@ class AddGithubRepository extends Scaffolder
             $this->exec('git init');
         }
 
-        $org = $this->command->confirm('Use organization?', false);
-        $private = $this->command->confirm('Make repository private?', true);
+        $credentials = collect($this->config('github_credentials') ?? []);
 
-        $username = $this->command->ask('GitHub username', trim(`git config --global user.name`));
-        $password = $this->command->secret('GitHub password');
-        $repo_name = $this->command->ask('Repository name', basename(getcwd()));
+        if ($credentials->count()) {
 
-        $u = $org ? ('orgs/'.$username) : ('user');
+            $logins = $credentials->pluck('username')->toArray();
+            $logins[] = '<comment>[add new login]</comment>';
+            $chosen = $this->command->choice('Which username to use?', $logins, count($logins) - 1);
 
-        $ch = curl_init('https://api.github.com/'.$u.'/repos');
-
-        curl_setopt($ch, CURLOPT_USERPWD, $username.":".$password);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
-        curl_setopt($ch, CURLOPT_USERAGENT, 'Afterflow');
-        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
-        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-        curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode([
-                'name' => $repo_name,
-                'private' => $private
-            ])
-        );
-
-        $json = curl_exec($ch);
-        curl_close($ch);
-
-        $j = json_decode($json, true);
-
-        if (isset($j['html_url'])) {
-            `git remote add origin git@github.com:$username/$repo_name.git`;
-            $this->command->line('Repository created: '.$j['html_url']);
-        } else {
-            $this->command->error('Failed to create repository: '.PHP_EOL.$json);
         }
 
+        if ($chosen == '<comment>[add new login]</comment>') {
 
+            $username = $this->command->ask('GitHub username', trim(`git config --global user.name`));
+            $password = $this->command->secret('GitHub password');
+
+            if ($this->command->confirm('Remember credentials?', true)) {
+                $credentials->add([
+                    'username' => $username,
+                    'password' => $password,
+                ]);
+
+                $this->config('github_credentials', $credentials->toArray());
+            }
+        } else {
+            $user = $credentials->firstWhere('username', $chosen);
+            $username = $user['username'];
+            $password = $user['password'];
+        }
+
+        $this->github = new Client();
+        $this->github->authenticate($username, $password, Client::AUTH_HTTP_PASSWORD);
+
+        $repo_name = $this->command->ask('Repository name', basename(getcwd()));
+
+        try {
+            while ($this->repositoryExists($repo_name)) {
+                $repo_name = $this->command->ask('Repository <comment>'.$repo_name.'</comment> exists. Pick another name: ',
+                    basename(getcwd()));
+            }
+
+            $private = $this->command->confirm('Make repository private?', true);
+
+            $j = $this->github->repos()->create($repo_name, '', '', !$private);
+
+            if (!isset($j['html_url'])) {
+                $this->command->error('Failed to create repository: '.PHP_EOL.print_r($j, true));
+                return $this->restart();
+            }
+
+        } catch (RuntimeException $e) {
+            $this->command->error('ERROR: '.$e->getMessage());
+            return $this->restart();
+        }
+
+        $this->exec('git remote add origin '.$j['git_url']);
+        $this->command->line('Repository created: '.$j['html_url']);
+
+    }
+
+    protected function repositoryExists($repo_name)
+    {
+        return collect($this->github->currentUser()->repositories())->pluck('name')->contains($repo_name);
     }
 
 }
